0e128c1f79f786ce5c38488293e65f79122ed85d
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2025-10-04 20:21:51 +0200

    changes

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -1,12 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { userApi } from "../services/Api";
-import { showNotification } from "../redux/slices/notificationsSlice.js";
-import * as authSelectors from "../redux/slices/authSelectors.js";
-import * as authOperations from "../redux/slices/authOperations";
-import * as authSlice from "../redux/slices/authSlice.js";
+import { userApi } from '../services/Api';
+import * as authSelectors from '../redux/slices/authSelectors.js';
+import * as authOperations from '../redux/slices/authOperations';
+import * as authSlice from '../redux/slices/authSlice.js';
+import { updateAvatar } from '../redux/slices/authSlice.js';
+import { refreshUser } from '../redux/slices/authOperations';
 
 export const useOwner = () => {
   const owner = useSelector(authSelectors.getUser);
@@ -17,31 +18,27 @@ export const useOwner = () => {
 export const useFollow = () => {
   const dispatch = useDispatch();
 
-  const onFollow = async (id) => {
+  const onFollow = async id => {
     try {
       await userApi.followUser(id);
-      dispatch(authSlice.updateFollowing(id));
-      dispatch(authSlice.updateUserProfile({ key: "following", value: 1 }));
-      dispatch(
-        showNotification({ type: "success", message: "You followed this user" })
+      await dispatch(authSlice.updateFollowing(id));
+      await dispatch(
+        authSlice.updateUserProfile({ key: 'following', value: 1 })
       );
     } catch (error) {
       console.log(error);
-      dispatch(showNotification({ type: "error", message: "Follow failed" }));
     }
   };
 
-  const onUnfollow = async (id) => {
+  const onUnfollow = async id => {
     try {
       await userApi.unfollowUser(id);
-      dispatch(authSlice.updateFollowing(id));
-      dispatch(authSlice.updateUserProfile({ key: "following", value: -1 }));
-      dispatch(
-        showNotification({ type: "info", message: "You unfollowed this user" })
+      await dispatch(authSlice.updateFollowing(id));
+      await dispatch(
+        authSlice.updateUserProfile({ key: 'following', value: -1 })
       );
     } catch (error) {
       console.log(error);
-      dispatch(showNotification({ type: "error", message: "Unfollow failed" }));
     }
   };
 
@@ -52,48 +49,15 @@ export const useUpdateAvatar = () => {
   const dispatch = useDispatch();
 
   const onUpdateAvatar = async ({ target }) => {
-    const file = target.files?.[0];
-    if (!file) return;
-
-    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
-    if (!allowedTypes.includes(file.type)) {
-      dispatch(
-        showNotification({
-          type: "error",
-          message: "Please upload a valid image (JPEG, PNG, WEBP)",
-        })
-      );
-      return;
-    }
-
-    if (file.size > 2 * 1024 * 1024) {
-      // >2MB
-      dispatch(
-        showNotification({
-          type: "error",
-          message: "File is too large (max 2MB)",
-        })
-      );
-      return;
-    }
+    const formData = new FormData();
+    formData.append("avatar", target.files[0]);
 
     try {
-      const formData = new FormData();
-      formData.append("avatar", file);
       const { data } = await userApi.updateAvatar(formData);
-
-      dispatch(authSlice.updateAvatar(data?.avatar));
-      dispatch(
-        showNotification({
-          type: "success",
-          message: "Avatar updated successfully!",
-        })
-      );
+      dispatch(updateAvatar(data?.avatar));
+      dispatch(refreshUser())
     } catch (error) {
-      console.error(error);
-      dispatch(
-        showNotification({ type: "error", message: "Avatar update failed" })
-      );
+      console.log(error);
     }
   };
 
