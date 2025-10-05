2de91b3cf7b1458ab13cf7b13fae58e196d65283
Serj Pyshkin <IndianaCornell@gmail.com>
2025-10-05 20:03:04 +0300

    fix api
    fix api

--- src/redux/slices/testimonialsOperations.js
+++ src/redux/slices/testimonialsOperations.js
@@ -4,7 +4,9 @@ export const getTestimonials = createAsyncThunk(
   "testimonials/getAll",
   async (_, thunkAPI) => {
     try {
-      const res = await fetch("http://localhost:3000/api/testimonials");
+      const res = await fetch(
+        "https://project-404team1-backend.onrender.com/api/testimonials"
+      );
       if (!res.ok) throw new Error(`Failed: ${res.status}`);
       const data = await res.json();
       return data;
