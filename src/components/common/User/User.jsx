2c9cadfe1dc774b4f520777a90817b531120299b
Serj Pyshkin <IndianaCornell@gmail.com>
2025-10-05 04:00:57 +0300

    fix css
    fix css

--- src/components/common/User/User.jsx
+++ src/components/common/User/User.jsx
@@ -1,14 +1,15 @@
-import styles from './User.module.css';
-import PathInfo  from '../PathInfo/PathInfo';
-import MainTitle  from '@components/common/Typography/MainTitle';
-import Subtitle from '@components/common/Typography/Subtitle';
-import Button from '@ui/Button';
-import Container  from '@/components/ui/Container/Container';
-import Loader from '@components/common/Loader/Loader';
+import styles from "./User.module.css";
+import PathInfo from "../PathInfo/PathInfo";
+import MainTitle from "@components/common/Typography/MainTitle";
+import Subtitle from "@components/common/Typography/Subtitle";
+import Button from "@ui/Button";
+import Container from "@/components/ui/Container/Container";
+import Loader from "@components/common/Loader/Loader";
 
-import UserInfo from '@/pages/User/UserInfo/UserInfo';
-import TabsList from '@/pages/User/TabList/TabsList';
-import LogOutModal from '@components/common/Modal/LogOutModal';
+import UserInfo from "@/pages/User/UserInfo/UserInfo";
+import TabsList from "@/pages/User/TabList/TabsList";
+import LogOutModal from "@components/common/Modal/LogOutModal";
+import ListItems from "@/pages/User/ListItems/ListItems";
 
 const User = ({
   isOwner,
@@ -22,7 +23,7 @@ const User = ({
   onOpenLogoutModal,
   onCloseLogoutModal,
 }) => {
-  const textButton = isOwner ? 'log out' : isFollow ? 'following' : 'follow';
+  const textButton = isOwner ? "log out" : isFollow ? "following" : "follow";
 
   const onButtonClick = () => {
     if (isOwner) {
@@ -41,8 +42,8 @@ const User = ({
       <Container>
         <PathInfo path="profile" />
         <div className={styles.texts}>
-          <MainTitle text="Profile" />
-          <Subtitle text="Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us." />
+          <MainTitle children="Profile" />
+          <Subtitle children="Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us." />
         </div>
         <div className={styles.content}>
           <div className={styles.info_wrapper}>
@@ -58,10 +59,7 @@ const User = ({
         </div>
       </Container>
       {isLogoutModal && (
-        <LogOutModal
-          isOpen={isLogoutModal}
-          onClose={onCloseLogoutModal}
-        />
+        <LogOutModal isOpen={isLogoutModal} onClose={onCloseLogoutModal} />
       )}
     </section>
   );
